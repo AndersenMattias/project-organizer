b476558dbcccddb1c75b1388cd3a91cf3d38fc5d
mattias.andersen@consid.se <mattias.andersen@consid.se>
2023-12-21 10:42:17 +0100

    feat: add express, pg with connection to database

---
+++ index.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+import express, { Express, Request, Response } from 'express'
+import cors from 'cors'
+
+dotenv.config()
+
+const app: Express = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', async (req: Request, res: Response) => {
+  res.json({ message: 'Hello from server side!' })
+})
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
