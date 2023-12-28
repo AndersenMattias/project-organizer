edfce0574dad54e430052f804bcb22595251cefc
mattias.andersen@consid.se <mattias.andersen@consid.se>
2023-12-28 08:43:01 +0100

    chore: add types, project and task

---
+++ client/src/types/projects.ts
@@ -0,0 +1,15 @@
+export type ProjectType = {
+  project_id: number
+  name: string
+  description: string
+  start_date: string
+  end_date: string
+  project_manager_id: number
+  status: string
+  image_url: string
+  client_name: string
+  client_email: string
+  client_phone: string
+  created_at: string
+  updated_at: string
+}
---
+++ client/src/types/tasks.ts
@@ -0,0 +1,9 @@
+export type TaskType = {
+  task_id: number
+  task_description: string
+  due_date: string
+  task_status: 'Not started' | 'In progress' | 'Completed' | 'On hold'
+  priority: 'Low' | 'Medium' | 'High'
+  employee_id: number
+  employee_name: string
+}
